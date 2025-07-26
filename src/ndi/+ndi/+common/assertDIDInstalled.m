f06fee5db1a5f26c10c40250b9a8da316d5ed7f8
ehennestad <ehennestad@gmail.com>
2025-07-26 14:42:16 +0200

    Squashed commit of the following:
    commit 2ae4cad8036a8bb69cb14eeff1394189512b1bdd
    Merge: 34e305677 2a4f569f8
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 14:26:58 2025 +0200
    
        Merge branch 'main' of https://github.com/ehennestad/NDI-matlab
    
    commit 34e305677825f47b7d6e6770459001d1adcb2707
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 14:26:56 2025 +0200
    
        Fix installation of docs requirements in build-docs action
    
    commit 2a4f569f81eb59f84ca133866441774f9fd1efaa
    Author: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Date:   Sat Jul 26 11:58:56 2025 +0000
    
        Update GitHub badges
    
    commit 3dc2133d5a33267f2b776403224b93fd80a2f2ef
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 13:50:55 2025 +0200
    
        DEBUG: DID not found in build-docs workflow
    
    commit 33e04009aa11b59b852163decae82d5c260cc4e4
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 13:28:49 2025 +0200
    
        Update assertDIDInstalled.m
    
    commit 03a88aa06c8541b0847036d8697317d10aa1fa60
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 13:25:54 2025 +0200
    
        DEBUG: DID not found in build-docs workflow
    
    commit b1cd72da80424bee66f180d43b0138c92a06ed1c
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 13:14:28 2025 +0200
    
        DEBUG: DID not found in build-docs workflow
    
    commit 185d40e327b267ea0dd55c9d8e2ca2020e51f480
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 13:08:43 2025 +0200
    
        DEBUG: DID not found in build-docs workflow
    
    commit aa9be354826c2da68a9c2755afa4d0f5c5c54c99
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:57:57 2025 +0200
    
        Update build-docs.yml
    
    commit 7072444364ca1b66f2a82eee7febd1fcb81f7593
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:54:15 2025 +0200
    
        Update build-docs.yml
    
    commit a9aae06e1d681e21e30a715b313fe658f6594e38
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:53:12 2025 +0200
    
        Update requirements.txt
    
    commit 3a7923aa9e08220ce9954dd5f7d5d4257395d553
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:46:59 2025 +0200
    
        Update build-docs.yml
    
    commit b4fddc46849582a4165c9da7b2b4e1831f9b530c
    Merge: 2d46fcbde 4794cdbcc
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:40:50 2025 +0200
    
        Merge branch 'main' of https://github.com/ehennestad/NDI-matlab
    
    commit 2d46fcbde2b9d2f983736094165bb5bc71bec904
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:40:38 2025 +0200
    
        Update build-docs workflow to install vhlab-toolbox requirement
    
    commit 4794cdbcc82a9c95409ebcdd3feb8ec1c686e0d7
    Author: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Date:   Sat Jul 26 10:34:44 2025 +0000
    
        Update GitHub badges
    
    commit 7f36853bd053570cec3b313c80143fda0f4333fd
    Merge: be30aea70 ff28775dc
    Author: ehennestad <ehennestad@gmail.com>
    Date:   Sat Jul 26 12:26:27 2025 +0200
    
        Merge branch 'add-docs-workflow'
    
    commit be30aea708c65381126455c71813c7c6b2881aaa
    Author: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Date:   Fri Jul 25 19:13:40 2025 +0000
    
        Update GitHub badges

--- src/ndi/+ndi/+common/assertDIDInstalled.m
+++ src/ndi/+ndi/+common/assertDIDInstalled.m
@@ -1,6 +1,6 @@
 function assertDIDInstalled()
     s = which('did.database');
-
+    
     if isempty(s)
         ME = MException("NDI:DIDNotFound", ...
             "DID-MATLAB was not found. Please ensure DID-MATLAB is installed and added to MATLAB's search path");
